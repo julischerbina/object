65abb6da4dcac5b22aeb1fe886906f538e1cd99c
jul <julia_sky@mail.ru>
2022-12-21 17:04:35 +0300

    Объекты и классы. ДЗ 2

--- src/Book.java
+++ src/Book.java
@@ -1,12 +1,33 @@
+import java.util.Objects;
+
 public class Book {
-    private String nameOfTheBook;
-    private Author author;
+    private final String nameOfTheBook;
+    private final Author author;
     private int publicationYear;
 
     public Book(String nameOfTheBook, Author author, int publicationYear) {
         this.nameOfTheBook = nameOfTheBook;
         this.author = author;
         this.publicationYear = publicationYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Книга: " + nameOfTheBook+ "; \n" + author.toString() + "; \n" +
+                "Год публикации: " + publicationYear + ".";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return nameOfTheBook.equals(book.nameOfTheBook) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameOfTheBook, author);
     }
 
     public String getNameOfTheBook() {
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         Author tolstoy = new Author("Лев", "Толстой");
+        Author tolstoy2 = new Author("Лев", "Толстой");
         Author gogol = new Author("Николай", "Гоголь");
+       // System.out.println(tolstoy.equals(tolstoy2));
         Book warAndPeace = new Book("Война и мир", tolstoy, 1990);
         Book viy = new Book("Вий", gogol, 1992);
+        Book viy2 = new Book("Вий", gogol, 1994);
+        System.out.println(viy.equals(viy2));
         viy.setPublicationYear(1995);
-        System.out.println("Книга: " + warAndPeace.getNameOfTheBook() + "; \nавтор: " + tolstoy.getNameAuthor() + " " + tolstoy.getSurnameAuthor() + "; \nгод публикации: " + warAndPeace.getPublicationYear() + ".");
-        System.out.println("Книга: " + viy.getNameOfTheBook() + "; \nавтор: " + gogol.getNameAuthor() + " " + gogol.getSurnameAuthor() + "; \nгод публикации: " + viy.getPublicationYear() + ".");
+        //System.out.println("Книга: " + warAndPeace.getNameOfTheBook() + "; \nавтор: " + tolstoy.getNameAuthor() + " " + tolstoy.getSurnameAuthor() + "; \nгод публикации: " + warAndPeace.getPublicationYear() + ".");
+        System.out.println(warAndPeace);
+        System.out.println(viy);
+      //  System.out.println("Книга: " + viy.getNameOfTheBook() + "; \nавтор: " + gogol.getNameAuthor() + " " + gogol.getSurnameAuthor() + "; \nгод публикации: " + viy.getPublicationYear() + ".");
     }
 }
